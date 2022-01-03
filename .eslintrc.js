cecce6c2c4555d974bd31c5f23c7162bef2b68e5
MarciusCoreolan <80627298+MarciusCoreolan@users.noreply.github.com>
2022-01-04 02:29:18 +0300

    finish base framework functional (#1)
    Co-authored-by: FeNRiR <germes955@gamail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'semi': 'off',
     'comma-dangle': 'off',
-    'require-jsdoc': 'off'
+    'require-jsdoc': 'off',
+    'operator-linebreak': 'off'
   }
 }
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,19 @@
+import {DomListener} from '@core/DomListener';
+
+export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+    this.name = options.name || ''
+  }
+
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+  init() {
+    this.initDomListener()
+  }
+  destroy() {
+    this.removeDomListener()
+  }
+}
